a597c27e58793b270efd210f6b26921a5fce9a90
Zach Wright <zachwrightdev@gmail.com>
2016-04-01 21:29:20 -0700

    now with color

--- draw.js
+++ draw.js
@@ -1,3 +1,28 @@
+/********** Controls **********/
+
+var canvas = document.getElementById("mainCanvas"),
+    btnClear = document.getElementById("btnClear"),
+    btnSave = document.getElementById("btnSave"),
+    btnLoad = document.getElementById("btnLoad"),
+    colorInput = document.getElementById('colorInput');
+
+var color = colorInput.value;
+
+btnClear.addEventListener("click", function () {
+    canvas.width = canvas.width;
+});
+
+btnSave.addEventListener("click", function () {
+    saveCanvas();
+});
+
+btnLoad.addEventListener("click", function () {
+    loadCanvas();
+});
+
+colorInput.addEventListener("input", function(){
+    return color = colorInput.value;
+})
 /********* Drawing Functionality **********/
 
 var canvas = document.getElementById('mainCanvas'),
@@ -19,10 +44,10 @@ canvas.addEventListener("mousedown", function (e) {
         x = pos.x,
         y = pos.y;
 
-    ctx.lineWidth = 6;
-    ctx.lineJoin = ctx.lineCap = 'round';
     drawing = true;
+    ctx.beginPath();
     ctx.moveTo(x, y);
+    
 });
 
 canvas.addEventListener("mousemove", function (e) {
@@ -31,34 +56,32 @@ canvas.addEventListener("mousemove", function (e) {
         y = pos.y;
 
     if (drawing) {
-        ctx.lineTo(x, y);
-        ctx.stroke();
+        marker(x,y);
     }
 });
 
 canvas.addEventListener("mouseup", function (e) {
     drawing = false;
+    ctx.closePath();
 });
 
+canvas.addEventListener("mouseout", function (e){
+    drawing = false;
+    ctx.closePath();
+})
 
-/********** Controls **********/
+/********** Brushes **********/
 
-var btnClear = document.getElementById("btnClear"),
-    btnSave = document.getElementById("btnSave"),
-    btnLoad = document.getElementById("btnLoad");
-
-btnClear.addEventListener("click", function (e) {
-    canvas.width = canvas.width;
-});
-
-btnSave.addEventListener("click", function (e) {
-    saveCanvas();
-});
-
-btnLoad.addEventListener("click", function(e){
-   loadCanvas(); 
-});
-
+function marker(x,y) {
+    ctx.lineWidth = 6;
+    ctx.lineJoin = ctx.lineCap = 'round';
+    ctx.lineTo(x, y);
+    ctx.strokeStyle = "#FFFFFF";
+    ctx.shadowBlur = 2;
+    ctx.shadowColor = color;
+    ctx.stroke();
+    // makes a cool start shape ctx.closePath();
+}
 
 /********** Local Storage **********/
 
